628018bb3ed5a32c9397ee1ebef74453d68ff859
icesousa <ice-l337elite@hotmail.com>
2023-03-21 00:11:48 -0300

    linter rules

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,277 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const KuraImc());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class KuraImc extends StatelessWidget {
+  const KuraImc({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+double peso = 80;
+double altura = 1.80;
+double imc = peso / (altura * altura);
+String _descricao = 'Normal';
+Color _containercolor = Colors.green.shade200;
+IconData _iconeImc = Icons.sentiment_very_satisfied_outlined;
+Color _fontecolor = Colors.black;
+bool visivel = true;
 
-  void _incrementCounter() {
+_showDicas(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('REQUER ATENÇÃO MÉDICA'),
+        content: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              Text(
+                  '- Consulte um médico: A primeira etapa para procurar ajuda para a obesidade é marcar uma consulta com um médico.'),
+              SizedBox(height: 8),
+              Text(
+                  '- Considere a terapia comportamental: A terapia comportamental pode ser útil para pessoas com obesidade, especialmente aquelas que têm problemas emocionais ou comportamentais relacionados à alimentação e ao exercício.'),
+              SizedBox(height: 8),
+              Text(
+                  '- Procure grupos de apoio: Grupos de apoio, como os de Comedores Compulsivos Anônimos, podem fornecer uma rede de suporte para pessoas que enfrentam problemas de obesidade.'),
+              SizedBox(height: 8),
+              Text(
+                '- Busque um nutricionista: Um nutricionista pode ajudar a pessoa a entender melhor sua alimentação.',
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Text(
+                  '- Evite dietas extremas: Dietas extremas, como aquelas que envolvem restrições calóricas muito severas, podem ser perigosas e não sustentáveis ​​a longo prazo')
+            ],
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Text('Fechar'),
+          ),
+        ],
+      );
+    },
+  );
+}
+
+class _HomePageState extends State<HomePage> {
+  buttonHelp() {
+    return Visibility(
+      maintainState: true,
+      maintainAnimation: true,
+      maintainSize: true,
+      visible: visivel,
+      child: AnimatedContainer(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        duration: const Duration(milliseconds: 300),
+        child: ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor:
+                    MaterialStatePropertyAll<Color>(_containercolor)),
+            onPressed: () {
+              _showDicas(context);
+            },
+            child: Text(
+              'Encontre  ajuda',
+              style: TextStyle(
+                  color: _fontecolor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w800),
+            )),
+      ),
+    );
+  }
+
+  _calcularIMC() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      imc = peso / (altura * altura);
+      if (imc < 18.5) {
+        _fontecolor = Colors.black;
+        _iconeImc = Icons.sentiment_neutral_outlined;
+        _descricao = 'Magreza';
+        _containercolor = Colors.yellow.shade300;
+        visivel = true;
+        return;
+      } else if (imc >= 18.5 && imc <= 24.9) {
+        _fontecolor = Colors.black;
+        _iconeImc = Icons.sentiment_very_satisfied_outlined;
+        _containercolor = Colors.green;
+        _descricao = 'Normal';
+        visivel = false;
+
+        return;
+      } else if (imc >= 25.0 && imc <= 29.9) {
+        _fontecolor = Colors.black;
+        _iconeImc = Icons.sentiment_dissatisfied_outlined;
+        _descricao = 'Sobrepeso';
+        _containercolor = Colors.orange.shade300;
+        visivel = false;
+        return;
+      } else if (imc >= 30.0 && imc <= 39.9) {
+        _fontecolor = Colors.black;
+        _iconeImc = Icons.sentiment_very_dissatisfied_outlined;
+        _containercolor = Colors.red.shade300;
+        _descricao = 'Obeso';
+        visivel = true;
+        return;
+      } else if (imc > 40.0) {
+        _iconeImc = Icons.sentiment_very_dissatisfied_outlined;
+
+        _containercolor = Colors.black;
+        _fontecolor = Colors.red;
+        _descricao = 'Obeso Grave';
+        visivel = true;
+        return;
+      }
+      return;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      backgroundColor: Colors.teal.shade200,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Icon(
+                _iconeImc,
+                size: 100,
+                color: _containercolor,
+              ),
+              const SizedBox(height: 24),
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 300),
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: _containercolor,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      _descricao,
+                      style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w900,
+                          color: _fontecolor),
+                    ),
+                    Text(
+                      '  ${imc.toStringAsFixed(1)}',
+                      style: TextStyle(
+                          fontSize: 40,
+                          fontWeight: FontWeight.w900,
+                          color: _fontecolor),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              buttonHelp(),
+              const SizedBox(height: 10),
+              Container(
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Colors.green.shade50),
+                child: Column(
+                  children: [
+                    Text(
+                      'Peso',
+                      style: TextStyle(
+                        color: Colors.green.shade900,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '${peso.toStringAsFixed(2)} kg',
+                      style: TextStyle(
+                        color: Colors.green.shade900,
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Slider.adaptive(
+                      activeColor: Colors.teal.shade400,
+                      min: 30,
+                      max: 200,
+                      value: peso,
+                      onChanged: (novopeso) {
+                        peso = novopeso;
+                        _calcularIMC();
+                      },
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 24),
+              Container(
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: Colors.green.shade50),
+                child: Column(
+                  children: [
+                    Text(
+                      'Altura',
+                      style: TextStyle(
+                        color: Colors.green.shade900,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '${altura.toStringAsFixed(2)} m',
+                      style: TextStyle(
+                        color: Colors.green.shade900,
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Slider.adaptive(
+                      activeColor: Colors.teal.shade400,
+                      min: 1,
+                      max: 2.3,
+                      value: altura,
+                      onChanged: (novaAltura) {
+                        altura = novaAltura;
+                        _calcularIMC();
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
